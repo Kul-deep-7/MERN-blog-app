b591a27f5b6d85da3e16611a4eef32e52bbcb2a1
Kul-deep-7 <sahiljangid321@gmail.com>
2026-01-21 14:34:12 +0530

    signup user controller

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,7 +1,44 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import User from "../models/user.models.js"
 
 const signupUser = asyncHandler(async(req,res)=>{
-    
+
+    const{name,password,username}=req.body
+
+     if (
+            [fullName, email, username, password].some( //The .some() method returns true if at least one element in an array satisfies a condition.
+                (field) => {
+                    //console.log("Checking field:", field) -> for debugging and checking using postman
+                    return field?.trim() === ""}//cb function.. field means 1 value at a time.
+             )//field?.trim():  ?. (optional chaining) prevents error if field is null or undefined. "Is this field empty after removing spaces"
+            ) {//Optional chaining (?.) allows safe access to a property or method of an object that may be null or undefined, without throwing an error.
+                throw new ApiError(400, "All fields are required")
+                console.log(field)
+            }
+
+    const existedUser = await User.findOne({username})
+
+    if (existedUser) {
+    console.log("User already exists:", existedUser)
+    throw new ApiError(409, "User with username already exists")
+    }
+
+    const user = await User.create({ //creates a mongodb User document..
+        name, //Comes from frontend. Stored as-is.
+        username: username.toLowerCase(),
+        password
+    })//creating user in db
+
+    const createdUser = await User.findById(user._id).select( //.select() is used when fetching data from MongoDB to decide:which fields you WANT(no -) & which fields you DON’T want(prefix with -)
+    "-password -refreshToken" )
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered successfully"))
+
 })
--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,19 +1,34 @@
+import { timeStamp } from "console";
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
     name:{
         type: String,
-        required: true
+        required: true,
+         trim: true
     },
     username:{
         type: String,
         required: true,
-        unique: true
+        unique: true,
+         trim: true
     },
     password:{
         type: String,
-        required: true
+        required: [true, 'Password is required']
+
     }
+},
+    {
+        timeStamp: true
+    }
+
+)
+
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next() //if password field is not modified, skip hashing and proceed to next middleware or save operation.
+//if password is modified (new user or password change), move to the next line to hash it
+    this.password = await bcrypt.hash(this.password, 10); //The plain password becomes a hashed string.
 })
 
 export const User = mongoose.model("User", userSchema)
