48b75148a2bf5de81fc088e33a265bc2c2afca39
Kul-deep-7 <sahiljangid321@gmail.com>
2026-01-27 18:59:02 +0530

    Refactor Categories component to update query parameter handling; change 'category' to 'categ' in URL & added comments

--- frontend/src/components/home/Categories.jsx
+++ frontend/src/components/home/Categories.jsx
@@ -20,16 +20,27 @@ const StyledLink = styled(Link) //doing this cuz text is underlined cuz of some 
 
 export default function Categories() {
 
-    //useSearchParams lets you read and write query parameters in the URL. Query params are the part after ?. EG: /?category=Music or /create?category=Tech
+    /*useSearchParams lets you read and write query parameters in the URL. Query params are the part after ?. EG: /?category=Music or /create?category=Tech
+    Why do we need ? in the URL?
+    Because ? means:“Everything after this is extra information, not a new page.”
+
+    URL has TWO parts: /create?category=sports
+    1. Path (page): /create
+    This decides which page/component opens.Okay, render <Create /> page
+
+    2. Query params (data): ?category=sports
+    This does NOT change the page. It only passes data to the same page. Hey, open /create… but keep sports in mind.
+
+    */
 
     const [searchParams] = useSearchParams()
-    const category = searchParams.get('category'); //From the URL, give me the value of category. So if the URL is: /?category=Sports. Then category === "Sports". If the URL is "/" category === null
-
+    const urlcategory = searchParams.get('categ'); //From the URL, give me the value of category. So if the URL is: /?category=Sports. Then category === "Sports". If the URL is "/" category === null
+    //what get() does is “From the URL, give me the value whose key is category.”
 
 
   return (
     <>
-    <StyledLink to={`/create?category=${category || ''}`}>
+    <StyledLink to={`/create?categ=${urlcategory || ''}`}> {/* If urlCategory exists, use it.If it doesn’t, use an empty string instead.*/}
         <StyledButton>Create Blog</StyledButton> 
 {/*“If I click Create Blog after selecting a category, the URL changes to /create?category=Sports so the Create Post page knows which category the user selected. */}
     </StyledLink>
@@ -51,7 +62,7 @@ export default function Categories() {
                         return (
                         <TableRow key={category.id}>
                             <TableCell>
-                                <StyledLink to={`/?category=${category.type}`}>
+                                <StyledLink to={`/?categ=${category.type}`}>
                                     {category.type}
 {/* We imported categories and mapped over them to display all category names, but we also wrapped each category inside a Link. When a category like Sports is clicked, React Router updates the URL to /?category=Sports*/}
                                 </StyledLink>
