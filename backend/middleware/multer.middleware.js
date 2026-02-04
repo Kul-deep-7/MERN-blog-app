ce837ffda6464ca0b395332f25e5016760a4046a
Kul-deep-7 <sahiljangid321@gmail.com>
2026-02-04 23:10:31 +0530

    fix: Correct file storage path in multer middleware to ensure proper file uploads

--- backend/middleware/multer.middleware.js
+++ backend/middleware/multer.middleware.js
@@ -4,7 +4,7 @@ import multer from 'multer';
 const storage = multer.diskStorage({ //diskstorage: stores files on disk(server's disk). alternate memoryStorage: stores files in memory(RAM)
   destination: function (req, file, cb) { //this function decides where the file will be stored. 
                                         // parameters: req: incoming http req. contains user info, headers, auth data, etc. file: metadata about the file eg orgininalname,size,etc. cb: callbackfunction which tells multer destination path & success or failure.
-    cb(null, "./backend/public") //null: no error. "public/temp" folder where file is saved.
+    cb(null, "./public") //null: no error. "public/temp" folder where file is saved.
   },
   filename: function (req, file, cb) { //This function decides WHAT the file will be named.
     cb(null, file.originalname )//null means no error. file.originalname: original name of the uploaded file.
